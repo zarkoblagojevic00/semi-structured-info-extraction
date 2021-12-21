9c8f7e06fbff097b4d19bff9b504b0b85b91296c
zarkoblagojevic00 <blagojevic.ra44.2018@uns.ac.rs>
2021-12-21 20:57:39 +0100

    Init commit

---
+++ process.py
@@ -0,0 +1,32 @@
+# import libraries here
+import datetime
+
+
+class Person:
+    """
+    Klasa koja opisuje prepoznatu osobu sa slike. Neophodno je prepoznati samo vrednosti koje su opisane u ovoj klasi
+    """
+    def __init__(self, name: str = None, date_of_birth: datetime.date = None, job: str = None, ssn: str = None,
+                 company: str = None):
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.job = job
+        self.ssn = ssn
+        self.company = company
+
+
+def extract_info(image_path: str) -> Person:
+    """
+    Procedura prima putanju do slike sa koje treba ocitati vrednosti, a vraca objekat tipa Person, koji predstavlja osobu sa licnog dokumenta.
+
+    Ova procedura se poziva automatski iz main procedure pa nema potrebe dodavati njen poziv u main.py
+
+    :param image_path: <str> Putanja do slike za obradu
+    :return: Objekat tipa "Person", gde su svi atributi setovani na izvucene vrednosti
+    """
+    person = Person('test', datetime.date.today(), 'test', 'test', 'test')
+
+    # TODO - Prepoznati sve neophodne vrednosti o osobi sa slike. Vrednosti su: Name, Date of Birth, Job,
+    #       Social Security Number, Company Name
+
+    return person
